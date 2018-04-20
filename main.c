81ef2436166e2d5e19ec8a4523cfef5b552bf84e
Christian-98 <christian-eroe@live.it>
2018-04-20 13:25:56 +0200

    Ho fatto una prima bozza di switch

--- main.c
+++ main.c
@@ -3,9 +3,28 @@
 
 int main() {
     bool flag=true;
-    printf("Hello World!\n");
-    if(flag){
-        printf("ciao\n");
-    }
+    char scelta;
+    printf("Smart Fridge 0.01 Alpha\n\n");
+    do{
+        printf("Premi 1 per la demo o 2 per avviare il programma\nAltrimenti premi 0 per uscire\n");
+        scanf("%1s", &scelta);
+        switch(scelta){
+            case '1':
+                printf("ciao");
+                //avvia funzione demo
+                break;
+            case '2':
+                //avvia il programma vero e proprio
+                break;
+            case '0':
+                //esci
+                flag=false;
+                break;
+            default:
+                //default non fare nulla
+                printf("Inserisci un valore corretto\n");
+                break;
+        }
+    }while(!flag);
     return 0;
 }
