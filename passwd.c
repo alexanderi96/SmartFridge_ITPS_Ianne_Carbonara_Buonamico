10ba7c43f2c698683268e4b1f8f812543a6d3af2
nikitashade <38554178+nikitashade@users.noreply.github.com>
2018-04-21 13:25:56 +0200

    mi da errori che non capisco, non le faccio da una vita le cose sui files ahaahahhah cmq appena puoi lo vediamo insieme con condivisione schermo, e ricordiamoci che appena facciamo una bozza e ci rendiamo conto che è fattibile dobbiamo sottoporla al prof, ma magari sarbbe utile che prima glielo chiediate voi dal vivo e vediate se vuole darvi consigli. a dopo :*

--- passwd.c
+++ passwd.c
@@ -1,4 +1,31 @@
-//
-// Created by aless on 21/04/2018.
-//
+#include "stdio.h"
+#include "string.h"
 
+
+char inserire_password(char x[]){
+    char psw[15];
+    printf("Inserire password");
+    scanf("s", psw);
+    return psw[15];
+}
+
+char modifica_password(char x[]){
+    char vecchia_password[15];
+    char nuova_password[15];
+    char nuova_password2[15];
+    printf("Inserire la vecchia password");
+    scanf("s", vecchia_password);
+    if(x==vecchia_password){
+        printf("Inserire nuova password");
+        scanf("%s", nuova_password);
+        printf("Inserire nuovamente la password");
+        scanf("%s", nuova_password2);
+        if(strcmp(nuova_password, nuova_password2)==0){
+            printf("Password modificata correttamente");
+        }else{
+            printf("Le due password inserite non corrispondono");
+        }
+    }else{
+        printf("La vecchia password inserita non è corretta");
+    }
+}
--- passwd.h
+++ passwd.h
@@ -1,8 +1,11 @@
-//
-// Created by aless on 21/04/2018.
-//
 
 #ifndef SMARTFRIDGE_ITPS_IANNE_CARBONARA_BUONAMICO_PASSWD_H
 #define SMARTFRIDGE_ITPS_IANNE_CARBONARA_BUONAMICO_PASSWD_H
 
 #endif //SMARTFRIDGE_ITPS_IANNE_CARBONARA_BUONAMICO_PASSWD_H
+
+
+#include "stdio.h"
+
+char inserire_password(char x[]);
+char modifica_password(char x[]);
