d4ce1e983b806e2b43194c74c7d44605d4af3a52
Christian-98 <christian-eroe@live.it>
2018-05-03 11:37:06 +0200

    aggiunta e modifica della funzione di ricerca di un parola, numero o altro nel file. Funzione da completare e da rivedere

--- file.c
+++ file.c
@@ -19,6 +19,38 @@ int createNewCsv(char nome[]){
 
 }
 
-int searchOnFile(char word[]){
+//Ricerca su file (la ricerca verrà eseguita per qualunque parola inserita dall'utente)
+int searchOnFile(char word[], char file[]){
+    FILE *pf;
+    pf=fopen(file,"r");
+    char buffer[2048];
 
+    if(pf==NULL){
+        printf("Impossibile leggere il file\n");
+        return 0;
+    }else{
+        printf("Ricerca della parola nel file\n");
+        while((fgets(buffer,2048,pf))!=NULL){
+            //Ricerca della parola digitata precedentemente
+            for(int i=0;i<buffer[i]!='\0';i++){
+                if(buffer[i]==word[0]){
+                    if(ricerca(buffer,word,i)==1){
+                        //Parola trovata e avvio della funzione ricerca
+                    }
+                }
+            }
+        }
+    }
+    fflush(stdin);
+    getchar();
+    return 0;
+}
+
+int ricerca(const char *buffer, const char *word, int i){
+    for(;*word!='\0';buffer++,word++){
+        if(*(word+buffer)!=*word){
+            return 0;
+        }
+    }
+    return 1;
 }
